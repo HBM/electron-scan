b555c4db99c75c851df51ed16a6f845fe21f7523
Florian Schopp <schopp@hbkworld.com>
2025-03-24 11:27:10 +0100

    Updated to 4 messages

---
+++ mock/messages.ts
@@ -0,0 +1,127 @@
+export const messages = [
+  {
+    jsonrpc: '2.0',
+    method: 'announce',
+    params: {
+      apiVersion: '1.0',
+      device: {
+        familyType: 'QuantumX',
+        firmwareVersion: '4.46.18.0',
+        name: 'MX1601B_Advantage',
+        type: 'MX1601',
+        uuid: '0009E50046CC'
+      },
+      expiration: 15,
+      netSettings: {
+        interface: {
+          configurationMethod: 'manual',
+          description: 'ethernet backplane side',
+          ipv4: [
+            { address: '172.19.106.101', netmask: '255.255.0.0' },
+            { address: '169.254.141.62', netmask: '255.255.0.0' }
+          ],
+          ipv6: [{ address: 'fe80::209:e5ff:fe00:46cc', prefix: 64 }],
+          name: 'eth0',
+          type: 'ethernet'
+        }
+      },
+      services: [
+        { port: 7411, type: 'daqStream' },
+        { port: 5001, type: 'hbmProtocol' },
+        { port: 80, type: 'http' },
+        { port: 11122, type: 'jetd' },
+        { port: 22, type: 'ssh' }
+      ]
+    }
+  },
+  {
+    jsonrpc: '2.0',
+    method: 'announce',
+    params: {
+      apiVersion: '1.0',
+      device: {
+        familyType: 'SmartTorque',
+        firmwareVersion: 'v1.0.5-4204\n',
+        label: 'test1',
+        name: 'T100',
+        stator: { identNr: 'test1', kCode: 'K-T100-test1-test2-test2-U' },
+        type: 'T100',
+        uuid: '0009E50092B5'
+      },
+      expiration: 15,
+      netSettings: {
+        interface: {
+          ipv4: [{ address: '172.19.211.95', netmask: '255.255.0.0' }],
+          ipv6: [{ address: 'fe80::209:e5ff:fe00:92b5', prefix: 64 }],
+          name: 'end0',
+          type: 'ethernet'
+        }
+      },
+      services: [
+        { port: 7416, type: 'daqStream' },
+        { port: 80, type: 'http' },
+        { port: 11122, type: 'jetd' },
+        { path: '/api/jet/', port: 11123, type: 'jetws' },
+        { port: 22, type: 'ssh' }
+      ]
+    }
+  },
+  {
+    jsonrpc: '2.0',
+    method: 'announce',
+    params: {
+      apiVersion: '1.0',
+      device: {
+        familyType: 'digiBOX',
+        firmwareVersion: '1.0.2+20250311',
+        name: 'digiBOX-Web-A2E9',
+        type: 'DBX4PADIEI',
+        uuid: '0009E5AAA2E9'
+      },
+      netSettings: {
+        interface: {
+          name: 'ETH0',
+          type: 'ethernet',
+          description: 'Primary Ethernet Port',
+          configurationMethod: 'dhcp',
+          ipv4: [{ address: '172.19.190.76', netmask: '255.255.0.0' }]
+        }
+      },
+      expiration: 30,
+      services: [
+        { port: 80, path: '/stream', type: 'daqStreamws' },
+        { port: 80, type: 'http' },
+        { port: 80, type: 'jetws', path: '/jet/canopen' }
+      ]
+    }
+  },
+  {
+    jsonrpc: '2.0',
+    method: 'announce',
+    params: {
+      apiVersion: '1.0',
+      device: {
+        familyType: 'BMX',
+        firmwareVersion: '0.2.0+20250324',
+        name: 'bmx60-9BEB-Carrier-NR3-Gianni',
+        type: 'BMXenabler',
+        uuid: '0009E5AA9BEB'
+      },
+      netSettings: {
+        interface: {
+          name: 'ETH0',
+          type: 'ethernet',
+          description: 'Primary Ethernet Port',
+          configurationMethod: 'dhcp',
+          ipv4: [{ address: '172.19.191.214', netmask: '255.255.0.0' }]
+        }
+      },
+      expiration: 30,
+      services: [
+        { port: 80, type: 'http' },
+        { port: 80, type: 'jetws', path: '/jet/canopen' },
+        { port: 80, type: 'daqStreamws', path: '/stream' }
+      ]
+    }
+  }
+]
--- mock/mock.ts
+++ mock/mock.ts
@@ -1,4 +1,5 @@
 import dgram, { type Socket } from 'dgram'
+import { messages } from './messages'
 const announceSocket = dgram.createSocket({ type: 'udp4', reuseAddr: true })
 const configSocket = dgram.createSocket({ type: 'udp4', reuseAddr: true })
 // Announce socket
@@ -16,45 +17,10 @@ configSocket.on('message', (msg) => {
   console.log('Received configuration:')
   console.log(JSON.parse(msg.toString()))
 })
-
+let msgIdx = 0
 // Announcement broadcasting
 setInterval(() => {
-  const message = {
-    jsonrpc: '2.0',
-    method: 'announce',
-    params: {
-      apiVersion: '1.0',
-      device: {
-        familyType: 'QuantumX',
-        firmwareVersion: '4.46.18.0',
-        name: 'MX1601B_Advantage',
-        type: 'MX1601',
-        uuid: '0009E50046CC'
-      },
-      expiration: 15,
-      netSettings: {
-        interface: {
-          configurationMethod: 'manual',
-          description: 'ethernet backplane side',
-          ipv4: [
-            { address: '172.19.106.101', netmask: '255.255.0.0' },
-            { address: '169.254.141.62', netmask: '255.255.0.0' }
-          ],
-          ipv6: [{ address: 'fe80::209:e5ff:fe00:46cc', prefix: 64 }],
-          name: 'eth0',
-          type: 'ethernet'
-        }
-      },
-      services: [
-        { port: 7411, type: 'daqStream' },
-        { port: 5001, type: 'hbmProtocol' },
-        { port: 80, type: 'http' },
-        { port: 11122, type: 'jetd' },
-        { port: 22, type: 'ssh' }
-      ]
-    }
-  }
-
-  console.log('sending')
-  announceSocket.send(JSON.stringify(message), 31416, '239.255.77.76')
+  console.log('sending', messages[msgIdx].params.device.name)
+  announceSocket.send(JSON.stringify(messages[msgIdx]), 31416, '239.255.77.76')
+  msgIdx = ++msgIdx % messages.length
 }, 2000)
